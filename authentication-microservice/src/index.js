d5aef4a9ad8e139b8f1fbb85e2b6f3110fb8fa45
Sayuru Bopitiya <sayururehan77@gmail.com>
2024-05-01 11:48:38 +0530

    added authentication service

---
+++ authentication-microservice/src/index.js
@@ -0,0 +1,35 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { config } from "dotenv";
+import express from "express";
+import { connectDB } from "../configs/DBConnect.js";
+import { login, register } from "./controllers/auth.controller.js";
+
+
+config();
+
+export const authService = express();
+authService.use(cookieParser());
+authService.use(cors());
+authService.use(express.json());
+
+const port = process.env.AUTH_PORT;
+
+// Start the server after connecting to the database
+connectDB()
+  .then(() => {
+    authService.listen(port, () => {
+      console.log(`Auth server running on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+authService.post("/test", (req, res) => {
+  console.log(`Received request to auth server from gateway`);
+  res.status(200).send("Response from auth server");
+});
+
+authService.post("/login", login)
+authService.post("/register", register)
