180d0fad9cbf4893fb122f8a6f5f8132af5508ed
Sandani Thilakarathna <127828782+Sandani2000@users.noreply.github.com>
2024-05-07 02:13:05 +0530

    add controllers

---
+++ learner-microservice/controllers/learnerController.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const Learner = require("../models/learnerSchema");
+const Course = require("../models/courseSchema");
+
+// Enroll student into a course
+router.post("/enroll", async (req, res) => {
+  try {
+    const { learnerId, courseId } = req.body;
+
+    // Check if the learner and course exist
+    const learner = await Learner.findById(learnerId);
+    const course = await Course.findById(courseId);
+
+    if (!learner) {
+      return res.status(404).json({ error: "Learner not found" });
+    }
+
+    if (!course) {
+      return res.status(404).json({ error: "Course not found" });
+    }
+
+    // Check if the learner is already enrolled in the course
+    if (learner.enrolledCourses.includes(courseId)) {
+      return res
+        .status(400)
+        .json({ error: "Learner is already enrolled in this course" });
+    }
+
+    // Add the course to the learner's enrolledCourses array
+    learner.enrolledCourses.push(courseId);
+    await learner.save();
+
+    res.status(200).json({ message: "Student enrolled successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+module.exports = router;
