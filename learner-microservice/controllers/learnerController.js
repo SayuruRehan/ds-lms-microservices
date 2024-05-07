f417c20cf7b9a5c42a5c118da0983461e77b1887
Sandani Thilakarathna <127828782+Sandani2000@users.noreply.github.com>
2024-05-08 03:10:29 +0530

    create unenroll course api

--- learner-microservice/controllers/learnerController.js
+++ learner-microservice/controllers/learnerController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Learner = require("../models/learnerSchema");
 
-// Enroll course
+// ---------------------------- Enroll -------------------------------------
 router.post("/course/enroll", async (req, res) => {
   try {
     const { learnerId } = req.body;
@@ -31,6 +31,40 @@ router.post("/course/enroll", async (req, res) => {
     await learner.save();
 
     res.status(200).json({ message: "Student enrolled successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// --------------------------- Unenroll -------------------------------------
+router.post("/course/unenroll", async (req, res) => {
+  try {
+    const { learnerId, courseId } = req.body;
+
+    // Check if the learner exists
+    const learner = await Learner.findById(learnerId);
+
+    if (!learner) {
+      return res.status(404).json({ error: "Learner not found" });
+    }
+
+    // Check if the learner is enrolled in the course - if not findIndex return -1
+    const index = learner.enrolledCourses.findIndex(
+      (course) => course.courseId.toString() === courseId
+    );
+
+    if (index === -1) {
+      return res
+        .status(400)
+        .json({ error: "Learner is not enrolled in this course" });
+    }
+
+    // Remove the course from the learner's enrolledCourses array
+    learner.enrolledCourses.splice(index, 1);
+    await learner.save();
+
+    res.status(200).json({ message: "Student unenrolled successfully" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
