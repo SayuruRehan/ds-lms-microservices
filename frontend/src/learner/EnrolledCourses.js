4a151b002d555b3bff0a0afef4165eb96929c602
Sandani Thilakarathna <127828782+Sandani2000@users.noreply.github.com>
2024-05-10 14:04:13 +0530

    Modify Enrolled Courses Page

--- frontend/src/learner/EnrolledCourses.js
+++ frontend/src/learner/EnrolledCourses.js
@@ -1,20 +1,44 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const EnrolledCourses = () => {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    const fetchCourses = async () => {
+    const fetchEnrolledCourses = async () => {
       try {
-        const response = await axios.get("http://localhost:4002/learner/enrollments/614f55396a149b001f8a652f");
-        setCourses(response.data.enrolledCourses);
+        const response = await axios.get(
+          "http://localhost:4002/learner/enrollments/614f55396a149b001f8a652f"
+        );
+        const enrolledCourses = response.data.enrolledCourses;
+        // const courseDetailsPromises = enrolledCourses.map(course => {
+        //     console.log("Waiting for course api response");
+        //   return axios.get(`http://localhost:4003/api/v1/course/get/:${course.courseId}`);
+
+        // });
+
+        //const courseDetailsResponses = await Promise.all(courseDetailsPromises);
+        const combinedCourses = enrolledCourses;
+        // const combinedCourses = enrolledCourses.map((course, index) => {
+        //   const courseDetails = courseDetailsResponses[index].data;
+        //   return {
+        //     ...course,
+        //     name: courseDetails.name,
+        //     thumbnail: courseDetails.thumbnail,
+        //     totalLessons: courseDetails.totalLessons
+        //   };
+        // });
+
+        setCourses(combinedCourses);
+        
       } catch (error) {
         console.error("Error fetching enrolled courses:", error);
       }
     };
 
-    fetchCourses();
+    fetchEnrolledCourses();
+    console.log(courses)
   }, []);
 
   return (
@@ -31,6 +55,7 @@ const EnrolledCourses = () => {
           Completed Courses
         </button>
       </div>
+      
       {/* Display enrolled courses */}
       <div className="grid grid-cols-1 gap-4 p-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {courses.map((course) => (
@@ -45,13 +70,14 @@ const EnrolledCourses = () => {
             />
             <div className="px-6 py-4">
               <h3 className="mb-2 text-lg font-semibold text-gray-900">
+                Course Name
                 {course.name}
               </h3>
               <p className="mb-2 text-gray-700">
-                Total Lessons: {course.totalLessons}
+                Total Lessons: {course.totalLessons} Count
               </p>
               <p className="mb-2 text-gray-700">
-                Completed Lessons: {course.completedLessons}
+                Completed Lessons: {course.lessonsCompleted.length}
               </p>
               <div className="h-4 mb-2 overflow-hidden bg-gray-200 rounded-lg">
                 <div
@@ -60,9 +86,15 @@ const EnrolledCourses = () => {
                 ></div>
               </div>
               <p className="mb-2 text-gray-700">{course.progress}% Completed</p>
-              <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline">
+              <Link
+                to={{
+                  pathname: `/courses/${course.courseId}`,
+                  state: { course },
+                }}
+                className="block px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+              >
                 Continue Learning
-              </button>
+              </Link>
             </div>
           </div>
         ))}
