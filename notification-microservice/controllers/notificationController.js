1519a7ecacc13bc067560fc1d77a8914f1c4c113
Kavindi Fernando <it21388248@my.sliit.lk>
2024-05-12 23:57:53 +0530

    Create Notification Microservice

---
+++ notification-microservice/controllers/notificationController.js
@@ -0,0 +1,26 @@
+const Notification = require("../models/notification");
+
+// Controller function for creating a new notification
+exports.createNotification = async (req, res) => {
+  try {
+    const { title, message, role } = req.body;
+
+    const newNotification = new Notification({
+      title,
+      message,
+      role,
+    });
+
+    await newNotification.save();
+
+    res
+      .status(201)
+      .json({
+        message: "Notification created successfully",
+        notification: newNotification,
+      });
+  } catch (error) {
+    console.error("Error creating notification:", error);
+    res.status(500).json({ message: "Error creating notification" });
+  }
+};
