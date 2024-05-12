7eb8b52d343fbf826379212e8c977fe72b69400f
Kavindi Fernando <it21388248@my.sliit.lk>
2024-05-13 00:34:32 +0530

    add noilmailer

--- notification-microservice/controllers/notificationController.js
+++ notification-microservice/controllers/notificationController.js
@@ -1,4 +1,31 @@
 const Notification = require("../models/notificationSchema");
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (to, subject, text) => {
+  // Create a Nodemailer transporter
+  let transporter = nodemailer.createTransport({
+    host: "smtp.example.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "yourusername@example.com",
+      pass: "yourpassword",
+    },
+  });
+
+  // Define email options
+  let mailOptions = {
+    from: "yourusername@example.com",
+    to: to,
+    subject: subject,
+    text: text,
+  };
+
+  // Send email
+  let info = await transporter.sendMail(mailOptions);
+
+  console.log("Message sent: %s", info.messageId);
+};
 
 // Controller function for creating a new notification
 exports.createNotification = async (req, res) => {
@@ -12,6 +39,13 @@ exports.createNotification = async (req, res) => {
     });
 
     await newNotification.save();
+
+    // Send email notification
+    await sendEmail(
+      "recipient@example.com",
+      "New Notification",
+      `Title: ${title}\nMessage: ${message}`
+    );
 
     res.status(201).json({
       message: "Notification created successfully",
