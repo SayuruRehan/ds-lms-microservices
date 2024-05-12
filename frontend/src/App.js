cdb70da4beb0ed620ae057b64fca70e3c92c5a3d
udanthaisuru@gmail.com <udanthaisuru@gmail.com>
2024-05-12 21:38:26 +0530

    Design home and main navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, {useState} from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/Background/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,53 +12,47 @@ import AddCourseForm from "./course/AddCourseForm";
 import CourseList from "./course/CourseList";
 import AdminCourseView from "./course/AdminCourseView";
 import EditCourseForm from "./course/EditCourseForm";
-import AllCourses from "./learner/AllCoursesList";
 import Success from "./learner/Success";
 import Unsuccess from "./learner/Unsuccess";
 import Enroll from "./learner/Enroll";
+import Header from "./components/Header";
 
 export default function App() {
   const heroData = [
-    { text1: "Learn. Explore. ", text2: "Grow. Thrive" },
-    { text1: "Knowledge at ", text2: "Your Fingertips" },
-    { text1: "Your Gateway", text2: "to Learning" },
+    {text1: "Learn. Explore. ", text2: "Grow. Thrive"},
+    {text1: "Knowledge at ", text2: "Your Fingertips"},
+    {text1: "Your Gateway", text2: "to Learning"},
   ];
 
   const [heroCount, setHeroCount] = useState(0);
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              heroCount={heroCount}
-              heroData={heroData}
-              setHeroCount={setHeroCount}
-            />
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
 
-        <Route path="/addCourse" element={<AddCourseForm />} />
-        <Route path="/list" element={<CourseList />} />
-        {/* <Route path="/video" element={<UploadVideo />} />
+          <Route path="/addCourse" element={<AddCourseForm />} />
+          <Route path="/list" element={<CourseList />} />
+          {/* <Route path="/video" element={<UploadVideo />} />
         <Route path="/vlist" element={<LectureVideos />} /> */}
 
-        {/* Learner Routes */}
-        <Route path="/enrolledCourses" element={<EnrolledCourses />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/courses/:courseID" element={<CoursePage />} />
-        <Route path="/all" element={<Enroll />} />
-        <Route path="/enroll/success" element={<Success />} />
-        <Route path="/enroll/unsuccess" element={<Unsuccess />} />
+          {/* Learner Routes */}
+          <Route path="/enrolledCourses" element={<EnrolledCourses />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/courses/:courseID" element={<CoursePage />} />
+          <Route path="/all" element={<Enroll />} />
+          <Route path="/enroll/success" element={<Success />} />
+          <Route path="/enroll/unsuccess" element={<Unsuccess />} />
 
-        <Route path="/adminList" element={<AdminCourseView />} />
-        <Route path="/edit/:courseId" element={<EditCourseForm />} />
-      </Routes>
-    </Router>
+          <Route path="/adminList" element={<AdminCourseView />} />
+          <Route path="/edit/:courseId" element={<EditCourseForm />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
